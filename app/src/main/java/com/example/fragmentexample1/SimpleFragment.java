ae851934e37ce5fa41367c1e29adea6500971324
Olbabej <0lbabej@gmail.com>
2022-07-28 11:23:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fragmentexample1/SimpleFragment.java
@@ -0,0 +1,53 @@
+package com.example.fragmentexample1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    public SimpleFragment() {
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static SimpleFragment newInstance(String param1, String param2) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+       final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+       return rootView;
+
+}
+}
