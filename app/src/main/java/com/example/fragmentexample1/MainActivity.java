497cd88e0933b6cc484d155e49a474b04481d2e0
Olbabej <0lbabej@gmail.com>
2022-08-01 12:35:15 +0200

    Added button that close and open Fragment

--- app/src/main/java/com/example/fragmentexample1/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1/MainActivity.java
@@ -5,18 +5,38 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private boolean isFragmentDisplayed = false;
+    static final String STATE_FRAGMENT = "state_of_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.btn_open);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed){
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
+
+        if (savedInstanceState != null){
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+        if (isFragmentDisplayed){
+            mButton.setText("Close");
+            }
+        }
     }
         public void displayFragment() {
             SimpleFragment simpleFragment = SimpleFragment.newInstance();
@@ -38,5 +58,10 @@ public class MainActivity extends AppCompatActivity {
             }
             mButton.setText("Open");
             isFragmentDisplayed = false;
+        }
+
+        public void onSaveInstanceState(Bundle savedInstanceState) {
+            savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+            super.onSaveInstanceState(savedInstanceState);
         }
 }
